9eea09fa484165a94467e481df21cdb2b5c529b6
Yanica <yanica_zafirova@abv.bg>
2023-03-21 14:48:42 +0200

    Create Footer Component with basic css

---
+++ src/components/common/footer/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <h2>Copyright &copy; Book Store</h2>
+        </div>
+    );
+};
