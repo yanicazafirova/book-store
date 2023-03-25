806dce1e9b0bdfdc950d52691dd829867b1e64ed
Yanica <yanica_zafirova@abv.bg>
2023-03-25 18:28:59 +0200

    Add logout functionality

--- src/components/logout/Logout.js
+++ src/components/logout/Logout.js
@@ -1,6 +1,22 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+import * as authService from '../../services/authService';
+
 export const Logout = () => {
-    return (
-        <>
-        </>
-    );
+    const { user, userLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        authService.logout(user.accessToken)
+            .then(() => {
+                userLogout();
+                localStorage.clear();
+                navigate('/');
+            })
+            
+    });
+
+    return null;
 };
