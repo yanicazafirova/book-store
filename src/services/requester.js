6b7a3b1228bdd667d6fe78493c9becca26b5b7d9
Yanica <yanica_zafirova@abv.bg>
2023-03-25 18:34:29 +0200

    Create global requester

---
+++ src/services/requester.js
@@ -0,0 +1,45 @@
+const request = async (method, url, data) => {
+    try {
+        const user = localStorage.getItem('auth');
+        const auth = JSON.parse(user || '{}');
+
+        let headers = {}
+
+        if (auth.accessToken) {
+            headers['X-Authorization'] = auth.accessToken;
+        }
+
+        let buildRequest;
+
+        if (method === 'GET') {
+            buildRequest = fetch(url, { headers });
+        } else {
+            buildRequest = fetch(url, {
+                method,
+                headers: {
+                    ...headers,
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+        }
+        const response = await buildRequest;
+
+        console.log(response);
+        // if (!response.ok) {
+        //     throw new Error('Failed to fetch!');
+        // }
+
+        const result = await response.json();
+
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const patch = request.bind(null, "PATCH");
+export const del = request.bind(null, "DELETE");
