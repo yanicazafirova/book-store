8ee620baa36c76105790ae0df1890b8142783056
Yanica <yanica_zafirova@abv.bg>
2023-03-25 18:25:22 +0200

    Update Register

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,19 +1,64 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './Register.module.css';
 
+import * as authService from '../../services/authService';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+
 export const Register = () => {
+    const { userLogin } = useContext(AuthContext);
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+        confirm: '',
+    });
+
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        setData(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const { email, password, confirm } = Object.fromEntries(new FormData(e.target));
+
+        if (email === '' || password === '' || confirm === '') {
+            return alert('All fields are required!');
+        }
+
+        if(password !== confirm){
+            return alert('Passwords do\'t match!');
+        }
+
+        try {
+            authService.register(email, password)
+                .then(userData => {
+                    userLogin(userData);
+                    navigate('/');
+                })
+                .catch(() => alert('Failed fetch!'))
+        } catch {
+            return alert('Email or password don\'t match!');
+        }
+    }
+
     return (
         <section className={styles.auth}>
-            <form id="login">
+            <form id="register" onSubmit={onSubmit}>
                 <div className={styles.container}>
 
                     <h1>Register</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="email@abv.bg" />
+                    <input type="email" id="email" name="email" placeholder="email@abv.bg" value={data.email} onChange={(e) => onChange(e)} />
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" placeholder="********" />
+                    <input type="password" id="login-password" name="password" placeholder="********" value={data.password} onChange={(e) => onChange(e)}/>
                     <label htmlFor="confirm-pass">Confirm Password:</label>
-                    <input type="password" id="confirm-password" name="confirm-password" placeholder="********" />
+                    <input type="password" id="confirm" name="confirm" placeholder="********" value={data.confirm} onChange={(e) => onChange(e)}/>
 
                     <div className={styles.error}>Error</div>
                     
