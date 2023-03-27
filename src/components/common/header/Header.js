8b2cb08083609f229ed2425b5263bed0a778dbe5
Yanica <yanica_zafirova@abv.bg>
2023-03-27 19:59:16 +0300

    Fix Header component

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,22 +1,29 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../services/authService';
 import styles from './Header.module.css';
 
 export const Header = () => {
+    const { user } = useContext(AuthContext);
+   
     return (
         <header className={styles.header}>
-            <h1 >
+            <h1 className={styles.title} >
                 <Link to="/" className={styles.title}>Book Store</Link>
             </h1>
             <nav className={styles.navbar}>
-                <div id="user" className={styles.user}>
-                    <Link to="/favourites">Your Favourites</Link>
-                    <Link to="/books/create">Create Book</Link>
-                    <Link to="/users/logout">Logout</Link>
-                </div>
-                <div id="guest" className={styles.guest} >
-                    <Link to="/users/login">Login</Link>
-                    <Link to="/users/register">Register</Link>
-                </div>
+                {user?.email ?
+                    <div id="user" className={styles.user}>
+                        <Link to="/favorites">Your Favourites</Link>
+                        <Link to="/books/create">Create Book</Link>
+                        <Link to="/users/logout">Logout</Link>
+                    </div>
+                    :
+                    <div id="guest" className={styles.guest} >
+                        <Link to="/users/login">Login</Link>
+                        <Link to="/users/register">Register</Link>
+                    </div>
+                }
             </nav>
         </header>
     );
