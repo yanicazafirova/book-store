a199c2c731119756b8e9697d97e6eda79a04bed0
Yanica <yanica_zafirova@abv.bg>
2023-03-27 19:59:58 +0300

    Add Favorite component

--- src/components/favourites/Favourites.js
+++ src/components/favourites/Favourites.js
@@ -1,7 +1,30 @@
-export const Favourites = () => {
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import styles from './Favorite.module.css';
+
+
+export const Favorites = () => {
+    const { user } = useContext(AuthContext);
+    
     return (
-        <div className="favourites">
-            <h2>Favourites</h2>
-        </div>
+        <div className={styles.favorites}> Favorites </div>
+        //     {favorites.map(book => (
+        //         <div key={book._id} className={styles["book-item"]}>
+        //             <div>
+        //                 <h4>{book.title}</h4>
+        //             </div>
+        //             <div>
+        //                 <img src={book.imageUrl} alt={book.title} />
+        //             </div>
+        //             {user.email && user._id !== book._ownerId ?
+        //                 <div>
+        //                     <button>Remove From Favorites</button>
+        //                 </div>
+        //                 : null
+        //             }
+        //         </div>
+        //     )
+        //     )}
+        // </div >
     );
 };
