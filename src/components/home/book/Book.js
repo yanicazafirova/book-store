d7ffb8f2132424551451046b00145d2ef20696ad
Yanica <yanica_zafirova@abv.bg>
2023-04-06 19:07:22 +0300

    Fix book component

--- src/components/home/book/Book.js
+++ src/components/home/book/Book.js
@@ -1,26 +1,15 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../../contexts/AuthContext";
-
-import styles from '../BookList.module.css';
 
 export const Book = (book) => {
-    const { user } = useContext(AuthContext);
 
     return (
-        <>
-            <div>
-                <h4>{book.title}</h4>
-            </div>
+
+        <div>
+            <h4>{book.title}</h4>
             <div>
                 <Link to={`/books/${book._id}`}><img src={book.imageUrl} alt={book.title} /></Link>
             </div>
-            {user.email && user._id !== book._ownerId ?
-                <div> 
-                    <button className={styles.button}>Add to Favorites</button>
-                </div>
-                : null
-            }
-        </>
+        </div>
+
     );
 };
