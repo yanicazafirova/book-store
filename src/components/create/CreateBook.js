43e39ac26279683903643ec39eb7b012f0ff2884
Yanica <yanica_zafirova@abv.bg>
2023-04-06 19:05:29 +0300

    Fix create book component

--- src/components/create/CreateBook.js
+++ src/components/create/CreateBook.js
@@ -2,6 +2,7 @@ import { useContext, useState } from 'react';
 
 import { BookContext } from '../../contexts/BookContext';
 import * as bookService from '../../services/bookService';
+import { CloseBtn } from '../common/close-button/CloseBtn';
 
 import styles from './CreateBook.module.css';
 
@@ -30,13 +31,13 @@ export const CreateBook = () => {
         if (bookData.title === '' || bookData.genre === '' || bookData.author === '' || bookData.imageUrl === '' || bookData.description === '') {
             return alert('All fields are required!');
         }
-        if(bookData.title.length < 5){
+        if (bookData.title.length < 5) {
             return alert('Book title should be at least 5 characters!');
-        }else if(bookData.genre.length < 3){
+        } else if (bookData.genre.length < 3) {
             return alert('Genre should be at least 3 characters!');
-        }else if(bookData.author.length < 4){
+        } else if (bookData.author.length < 4) {
             return alert('Author should be at least 4 characters!');
-        }else if(!bookData.imageUrl.startsWith('http')){
+        } else if (!bookData.imageUrl.startsWith('http')) {
             return alert('Image ulr should starts with http!');
         }
 
@@ -49,66 +50,38 @@ export const CreateBook = () => {
 
     return (
         <section id="create-page" className={styles.auth}>
-
-            <form id="create" onSubmit={onSubmit}>
-                <div className={styles.container}>
-                    <h1 className={styles.title}>Create Book</h1>
-
-                    <label htmlFor="book-title">Book title:</label>
-                    <input
-                        value={book.title}
-                        onChange={onChange}
-                        type="text"
-                        id="title"
-                        name="title"
-                        placeholder="Enter book title..."
-                    />
-                    <label htmlFor="category">Genre:</label>
-                    <input
-                        value={book.genre}
-                        onChange={onChange}
-                        type="text"
-                        id="genre"
-                        name="genre"
-                        placeholder="Enter book genre..."
-                    />
-                    <label htmlFor="author">Author:</label>
-                    <input
-                        value={book.author}
-                        onChange={onChange}
-                        type="text"
-                        id="author"
-                        name="author"
-                        placeholder="William Styron"
-                    />
-
-                    <label htmlFor="book-img">Image:</label>
-
-                    <input
-                        value={book.imageUrl}
-                        onChange={onChange}
-                        type="text"
-                        id="imageUrl"
-                        name="imageUrl"
-                        placeholder="Upload a photo..."
-                    />
-
-                    <label htmlFor="description">Book description:</label>
-
-                    <textarea
-                        name="description"
-                        id="description"
-                        value={book.description}
-                        onChange={onChange}
-                    />
-
-                    <input
-                        className={styles["btn-submit"]}
-                        type="submit"
-                        value="Create Book"
-                    />
-                </div>
-            </form>
+            <div className={styles.center}>
+                <CloseBtn />
+                <h1>Create book</h1>
+                <form onSubmit={onSubmit}>
+                    <div className={styles["txt_field"]}>
+                        <input type="text" name="title" value={book.title} onChange={onChange} />
+                        <span></span>
+                        <label>Title</label>
+                    </div>
+                    <div className={styles["txt_field"]}>
+                        <input type="text" name="genre" value={book.genre} onChange={onChange} />
+                        <span></span>
+                        <label>Genre</label>
+                    </div>
+                    <div className={styles["txt_field"]}>
+                        <input type="text" name="author" value={book.author} onChange={onChange} />
+                        <span></span>
+                        <label>Author</label>
+                    </div>
+                    <div className={styles["txt_field"]}>
+                        <input type="text" name="imageUrl" value={book.imageUrl} onChange={onChange} />
+                        <span></span>
+                        <label>Image</label>
+                    </div>
+                    <div className={styles["txt_field"]}>
+                        <input type="text" name="description" value={book.description} onChange={onChange} />
+                        <span></span>
+                        <label>Description</label>
+                    </div>
+                    <input type="submit" value="Create" />
+                </form>
+            </div>
         </section>
     );
 };
