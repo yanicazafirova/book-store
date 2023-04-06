a978d84e148ad1a7cd9fb96f2e50c8ae57ccf6a4
Yanica <yanica_zafirova@abv.bg>
2023-04-06 18:56:44 +0300

    Final updates App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { PrivateRoute } from './components/common/PrivateRoute';
-import { AuthProvider } from './context/AuthContext';
-import { BookProvider } from './context/BookContext';
+import { Routes, Route } from 'react-router-dom';
 
+import { AuthProvider } from './contexts/AuthContext';
+import { BookProvider } from './contexts/BookContext';
+
+import { PrivateRoute } from "./components/common/PrivateRoute";
+
+import { BookList } from './components/home/BookList';
+import { Login } from './components/login/Login';
+import { Register } from './components/register/Register';
+import { Header } from './components/common/header/Header';
+import { Footer } from './components/common/footer/Footer';
+import { CreateBook } from './components/create/CreateBook';
+import { NotFound } from './components/not-found/NotFound';
+import { Logout } from './components/logout/Logout';
+import { BookDetails } from './components/details/BookDetails';
+import { EditBook } from './components/edit/EditBook';
+
+//All book data which is rendering is in assets folder
 
 function App() {
-  
-  return (
-    <div className="App">
 
-    <AuthProvider >
-        <Header />
-        <BookProvider>
-            <Routes>
-                <Route path='/' element={<BookList />} />
-                <Route path='/users/login' element={<Login />} />
-                <Route path='/users/register' element={<Register />} />
-                <Route path='/books/:bookId' element={<BookDetails />} />
+    return (
+        <div className="App">
 
-                <Route element={<PrivateRoute />}>
-                    <Route path="/books/:bookId/edit" element={<EditBook />} />
-                    <Route path='/books/create' element={<CreateBook />} />
-                    <Route path='/favorites' element={<Favorites />} />
-                    <Route path='/users/logout' element={<Logout />} />
-                </Route>
-                <Route path='/*' element={<NotFound />} />
-            </Routes>
-        </BookProvider>
-        <Footer />
-    </AuthProvider>
+            <AuthProvider >
+                <Header />
+                <BookProvider>
+                    <Routes>
+                        <Route path='/' element={<BookList />} />
+                        <Route path='/users/login' element={<Login />} />
+                        <Route path='/users/register' element={<Register />} />
+                        <Route path='/books/:bookId' element={<BookDetails />} />
 
-</div >
-  );
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/books/:bookId/edit" element={<EditBook />} />
+                            <Route path='/books/create' element={<CreateBook />} />
+                            <Route path='/users/logout' element={<Logout />} />
+                        </Route>
+                        <Route path='/*' element={<NotFound />} />
+                    </Routes>
+                </BookProvider>
+                <Footer />
+            </AuthProvider>
+
+        </div >
+    );
 }
 
 export default App;
